19e5fecdef5d0c0beeb5712c6aed05e3a9126405
DongHoon <kdh4168@naver.com>
2021-01-18 20:34:32 +0900

    20210118

---
+++ src/Styles/Theme.js
@@ -0,0 +1,18 @@
+const BOX_BORDER = "1px solid #e6e6e6";
+const BORDER_RADIUS = "4px";
+export default {
+    bgColor: "#FAFAFA",
+    blackColor: "#262626",
+    darkGreyColor: "#999",
+    lightGreyColor: "#c7c7c7",
+    navyColor: "#364152",
+    ivoryColor: "#D8937B",
+    brownColor: "#B3795D",
+    starColor: "#F2CE5A",
+    boxBorder: "1px solid #e6e6e6",
+    borderRadius: "4px",
+    whiteBox: `Border:${BOX_BORDER};
+               border-Radius: ${BORDER_RADIUS};
+                background-color:white;
+                `
+};
